27c099fd494b850bef285e7f8d67a0bed44da9f4
Brendan Miller <brendan@swiftkids.ca>
2022-12-04 15:58:04 -0500

    Initial Commit

---
+++ CoreLocationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocationDemo
+//
+//  Created by Brendan Miller on 2022-12-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
